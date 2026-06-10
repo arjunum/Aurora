85150eb689f817fa437a07f9691217fad59e21e9
Arjun <arjunum88@gmail.com>
2026-06-10 20:29:36 +0530

    Initial Commit

---
+++ Aurora/AuroraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuroraApp.swift
+//  Aurora
+//
+//  Created by Arjun on 10/06/26.
+//
+
+import SwiftUI
+
+@main
+struct AuroraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
