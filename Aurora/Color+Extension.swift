75bc4c5f9337d03cdcf42de1a9fc3f420950549c
Arjun <arjunum88@gmail.com>
2026-06-11 11:07:57 +0530

    implementing dashboard ui

---
+++ Aurora/Color+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Extension.swift
+//  Aurora
+//
+//  Created by Arjun on 10/06/26.
+//
+
+import SwiftUI
+
+struct Color_Extension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Color_Extension()
+}
